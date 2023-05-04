c3b9e4b374f32a26dd535fb2ac89ed1f89d14b3c
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-04 21:35:03 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCqnGM1yAup6Jxt7YoTUf931bOBL3BB5LY",
-  authDomain: "chef-recipe-hunter-2dfd9.firebaseapp.com",
-  projectId: "chef-recipe-hunter-2dfd9",
-  storageBucket: "chef-recipe-hunter-2dfd9.appspot.com",
-  messagingSenderId: "1095988799438",
-  appId: "1:1095988799438:web:1c656393a6198216822d9f"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
